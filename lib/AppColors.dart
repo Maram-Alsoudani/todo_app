6dc441b62ba492d5e024bc304d33f569573caffc
Maram <maram.alsoudani@gmail.com>
2024-07-31 21:59:13 +0300

    Completed design and logic of home screen. Added Theme and colors of application. Connected to firebase firestore.

---
+++ lib/AppColors.dart
@@ -0,0 +1,15 @@
+import 'dart:ui';
+
+class AppColors {
+  static const Color primary_color = Color(0xff5D9CEC);
+  static const Color main_background_color_light = Color(0xffDFECDB);
+
+  static const Color white = Color(0xffFFFFFF);
+  static const Color black = Color(0xff303030);
+  static const Color green = Color(0xff61E757);
+  static const Color red = Color(0xffEC4B4B);
+  static const Color grey = Color(0xffC8C9CB);
+
+  static const Color main_background_color_dark = Color(0xff060E1E);
+  static const Color black_dark = Color(0xff141922);
+}
---
+++ lib/firebase_utils.dart
@@ -0,0 +1,31 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+import 'Task.dart';
+
+class FirebaseUtils {
+  static CollectionReference<Task> getTasksCollection() {
+    return FirebaseFirestore.instance.collection('tasks').withConverter<Task>(
+        fromFirestore: (snapshot, options) =>
+            Task.fromFireStore(snapshot.data()!),
+        toFirestore: (task, options) => task.toFireStore());
+  }
+
+  static Future<void> addTaskToFireStore(Task task) {
+    var taskCollection = getTasksCollection();
+    DocumentReference<Task> taskDocRef = taskCollection.doc();
+    task.id = taskDocRef.id;
+    return taskDocRef.set(task);
+  }
+
+  static Future<void> deleteTaskFromFireStore(Task task) {
+    return getTasksCollection().doc(task.id).delete();
+  }
+
+  static Future<void> updateTask(Task task, String key, dynamic newVal) {
+    return getTasksCollection()
+        .doc(task.id)
+        .update({key: newVal}).timeout(Duration(seconds: 1), onTimeout: () {
+      print("task updated successfully ");
+    });
+  }
+}
