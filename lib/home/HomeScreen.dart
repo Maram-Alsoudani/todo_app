4919c36e1e70017ad815de5e395172e16d4b7024
Maram <maram.alsoudani@gmail.com>
2024-07-31 21:46:59 +0300

    Init project. Added splash screen in light and dark mode. Added empty home screen.

---
+++ lib/home/HomeScreen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/cupertino.dart';
+
+class Homescreen extends StatelessWidget {
+  static const String screenRoute = "home_screen";
+
+  const Homescreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,35 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:to_do_app/home/HomeScreen.dart';
+
+import 'firebase_options.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  await FirebaseFirestore.instance.disableNetwork();
+
+  runApp(
+    const MyApp(),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      localizationsDelegates: AppLocalizations.localizationsDelegates,
+      supportedLocales: AppLocalizations.supportedLocales,
+      debugShowCheckedModeBanner: false,
+      initialRoute: Homescreen.screenRoute,
+      routes: {Homescreen.screenRoute: (context) => Homescreen()},
+    );
+  }
+}
